b29a22f3b201ecacc2953e714f37427a4f651475
camian0 <camian6@outlook.com>
2023-06-24 10:07:26 -0500

    Changed some features into producto model
    The product model was added features to hidde fields when queries are
    made.

--- api/app/Models/Product.php
+++ api/app/Models/Product.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
 }
