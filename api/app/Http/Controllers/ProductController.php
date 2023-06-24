a7865ad78bd1cf47f3b1095133a2a8d72925d5cf
camian0 <camian6@outlook.com>
2023-06-23 22:42:18 -0500

    Product controller was added
    The product controller was added to handle the diferent products
    queries. Structure of some methods also were added.

---
+++ api/app/Http/Controllers/ProductController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Response;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $response          = new Response();
+        $response->error   = false;
+        $response->message = 'Dentro de crear';
+        return $response->toJSON();
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
