ea896fc5c2515f2ff4c97ad028da5a3a3a6c66d8
camian0 <camian6@outlook.com>
2023-06-24 10:27:15 -0500

    create product was finished also validations
    Create product method was finished, also fields validation with customes
    messages was finished.

--- api/app/Http/Controllers/ProductController.php
+++ api/app/Http/Controllers/ProductController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ProductException;
+use App\Models\Product;
 use App\Models\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -25,10 +29,34 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $response          = new Response();
-        $response->error   = false;
-        $response->message = 'Dentro de crear';
-        return $response->toJSON();
+        $response = new Response();
+        try {
+            $validation = Validator::make($request->all(), [
+                'name'        => 'required',
+                'description' => 'required',
+                'price'       => 'required||numeric',
+            ]);
+
+            if ($validation->fails()) {
+                $response->error   = true;
+                $response->message = $validation->getMessageBag()->first() . " para producto.";
+                return $response->toJSON();
+            }
+
+            $productModel = new Product();
+            $productModel->fill($request->all());
+            $productModel->save();
+
+            $response->error   = false;
+            $response->message = 'Producto creado correctamente.';
+            return $response->toJSON();
+
+        } catch (ProductException $e) {
+            Log::error("Error en la creacion de producto\n" . $e->getMessage() . "\n\n" . $e->getTrace());
+            $response->error   = true;
+            $response->message = 'No se pudo crear el producto, verifica con el administrador el error.';
+            return $response->toJSON();
+        }
 
     }
 
--- api/resources/lang/es/validation.php
+++ api/resources/lang/es/validation.php
@@ -43,7 +43,7 @@ return [
     'digits_between'       => 'The :attribute must be between :min and :max digits.',
     'dimensions'           => 'The :attribute has invalid image dimensions.',
     'distinct'             => 'The :attribute field has a duplicate value.',
-    'email'                => 'The :attribute must be a valid email address.',
+    'email'                => 'El :attribute debe ser un correo válido.',
     'ends_with'            => 'The :attribute must end with one of the following: :values.',
     'enum'                 => 'The selected :attribute is invalid.',
     'exists'               => 'The selected :attribute is invalid.',
@@ -99,7 +99,7 @@ return [
     'multiple_of'          => 'The :attribute must be a multiple of :value.',
     'not_in'               => 'The selected :attribute is invalid.',
     'not_regex'            => 'The :attribute format is invalid.',
-    'numeric'              => 'The :attribute must be a number.',
+    'numeric'              => ' :attribute debe ser un número',
     'password'             => 'The password is incorrect.',
     'present'              => 'The :attribute field must be present.',
     'prohibited'           => 'The :attribute field is prohibited.',
@@ -107,7 +107,7 @@ return [
     'prohibited_unless'    => 'The :attribute field is prohibited unless :other is in :values.',
     'prohibits'            => 'The :attribute field prohibits :other from being present.',
     'regex'                => 'The :attribute format is invalid.',
-    'required'             => 'The :attribute field is required.',
+    'required'             => ':attribute es un campo obligatorio',
     'required_array_keys'  => 'The :attribute field must contain entries for: :values.',
     'required_if'          => 'The :attribute field is required when :other is :value.',
     'required_unless'      => 'The :attribute field is required unless :other is in :values.',
@@ -159,7 +159,9 @@ return [
      */
 
     'attributes'           => [
-
+        'name'        => "El nombre",
+        'description' => 'La descripcion',
+        'price'       => 'El precio',
     ],
 
 ];
