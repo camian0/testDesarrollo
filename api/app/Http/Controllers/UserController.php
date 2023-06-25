71cb8072d535c6262480b929bd4e4d100faa093e
camian0 <camian6@outlook.com>
2023-06-25 12:55:31 -0500

    User controller was added.
    The user controller was added, the method to create and get users
    were added.

---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\UserException;
+use App\Models\Response;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Show all the users
+     *
+     * @return App\Models\Response
+     */
+    public function get()
+    {
+        $response = new Response();
+        try {
+            $users = User::all();
+
+            $response->error   = false;
+            $response->data    = $users;
+            $response->message = 'Usuarios obtenidos correctamente.';
+            return $response->toJSON();
+
+        } catch (UserException $e) {
+            Log::warning("Error en obtener usuarios \n" . $e->getMessage() . "\n\n" . $e->getTraceAsString());
+            $response->error   = true;
+            $response->message = 'No se pudieron obtener los usuarios, verifica con el administrador el error.';
+            return $response->toJSON();
+        } catch (Exception $e) {
+            Log::error("Error en obtener usuarios \n" . $e->getMessage() . "\n\n" . $e->getTraceAsString());
+            $response->error   = true;
+            $response->message = 'No se pudieron obtener los usuarios, verifica con el administrador el error.';
+            return $response->toJSON();
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return App\Models\Response
+     */
+    public function store(Request $request)
+    {
+        $response = new Response();
+        try {
+            $validation = Validator::make($request->all(), [
+                'name'     => 'required',
+                'email'    => 'required|email',
+                'password' => 'required|date|min:8',
+            ]);
+
+            if ($validation->fails()) {
+                $response->error   = true;
+                $response->message = $validation->getMessageBag()->first() . " para la usuario.";
+                return $response->toJSON();
+            }
+            $user = new User();
+            $user->fill($request->all());
+
+            $response->error   = false;
+            $response->message = 'Usuario creado correctamente.';
+            return $response->toJSON();
+
+        } catch (UserException $e) {
+            Log::warning("Error al crear usuario \n" . $e->getMessage() . "\n\n" . $e->getTraceAsString());
+            $response->error   = true;
+            $response->message = 'No se pudo crear el usuario, verifica con el administrador el error.';
+            return $response->toJSON();
+        } catch (Exception $e) {
+            Log::error("Error al crear usuario \n" . $e->getMessage() . "\n\n" . $e->getTraceAsString());
+            $response->error   = true;
+            $response->message = 'No se pudo crear el usuario, verifica con el administrador el error.';
+            return $response->toJSON();
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return App\Models\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return App\Models\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
