3991a9ab0b06b901457391c147853c87d19e6e4e
camian0 <camian6@outlook.com>
2023-06-25 11:50:54 -0500

    Little change on autehnticate routes.

--- api/routes/api.php
+++ api/routes/api.php
@@ -21,9 +21,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::group([
-    'middleware' => 'jwtAuth',
-    'prefix'     => 'auth',
-
+    'prefix' => 'auth',
 ], function ($router) {
 
     Route::post('login', [AuthController::class, 'login']);
