2e4682094c129d324fc5fa9aa594cf12b53a53ac
camian0 <camian6@outlook.com>
2023-06-26 11:04:03 -0500

    Navbar place was chaged
    The navbar place was changed into app, to show correctly the
    component.

--- vueTest/src/router/index.js
+++ vueTest/src/router/index.js
@@ -9,21 +9,13 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      // component: Home
+      component: Home
     },
     {
       path: '/users',
       name: 'users',
       component: UserView
     },
-    {
-      // path: '/about',
-      // name: 'about',
-      // // route level code-splitting
-      // // this generates a separate chunk (About.[hash].js) for this route
-      // // which is lazy-loaded when the route is visited.
-      // component: () => import('../views/AboutView.vue')
-    }
   ]
 })
 
