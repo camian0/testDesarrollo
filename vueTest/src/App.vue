289b925d275511c2f863dd2d5404df12116e1377
camian0 <camian6@outlook.com>
2023-06-26 09:42:19 -0500

    Styles and configurations were changed.

--- vueTest/src/App.vue
+++ vueTest/src/App.vue
@@ -13,16 +13,6 @@ import Navbar from "../src/components/Navbar.vue";
 </template>
 
 <style scoped>
-body {
-  font-family: "Courier New", Courier, monospace !important;
-  min-height: 100vh;
-  color: #2c3e50;
-  background-color: rgb(224 231 231) !important;
-  transition: color 0.5s, background-color 0.5s;
-  line-height: 1.6;
-  font-size: 15px;
-  text-rendering: optimizeLegibility;
-}
 main {
   padding: 0px 25px;
 }
