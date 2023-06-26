76720763224fc4d001a2d999e2c27b6deb4bdacc
camian0 <camian6@outlook.com>
2023-06-25 20:24:10 -0500

    Element plus was used into app.

--- vueTest/src/main.js
+++ vueTest/src/main.js
@@ -1,11 +1,12 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from "element-plus";
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(ElementPlus);
 app.mount('#app')
