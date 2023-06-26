3c7bbdc80149992013673589f5dcff1fda1036ac
camian0 <camian6@outlook.com>
2023-06-26 05:42:06 -0500

    Some styles were modified.

--- vueTest/src/App.vue
+++ vueTest/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Home from "./views/Home.vue";
+import Navbar from "../src/components/Navbar.vue";
 </script>
 
 <template>
   <Home />
+  <Navbar />
   <RouterView />
 </template>
 
@@ -12,10 +14,10 @@ import Home from "./views/Home.vue";
 body {
   font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
+    sans-serif !important;
   min-height: 100vh;
   color: #2c3e50;
-  background-color: rgb(224 231 231);
+  background-color: rgb(224 231 231) !important;
   transition: color 0.5s, background-color 0.5s;
   line-height: 1.6;
   font-size: 15px;
